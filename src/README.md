0403cda8460d96030fa423b656df161356883f53
Bruno Pellizzetti <pelli.br@gmail.com>
2025-03-08 04:07:36 -0300

    readme

---
+++ src/README.md
@@ -0,0 +1,7 @@
+# TODO
+- Test HardBlock
+- Remove from HardBlock Method
+- Autocreate bot group
+- Block by time
+- Disco
+- Transcriber
--- src/whatsapp/qr.js
+++
@@ -1,8 +0,0 @@
-// const qrcode = require("qrcode-terminal");
-
-// function handleQR(qr) {
-//   console.log("Scan this QR code to login:");
-//   qrcode.generate(qr, { small: true });
-// }
-
-// module.exports = { handleQR };
